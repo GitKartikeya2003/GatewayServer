79d27a1d9a625646612eacd554c16ae08c12b74a
Kartikeya <kartikeyayash4@gmail.com>
2026-05-01 17:15:59 +0530

    Implementing Custom Routing in Spring Cloud gateway Server

--- gatewayserver/src/main/java/com/example/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/example/gatewayserver/GatewayserverApplication.java
@@ -3,13 +3,33 @@ package com.example.gatewayserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayserverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayserverApplication.class, args);
+    }
+
+
+
+    //Implementing Custom Routing in Spring Cloud gateway Server
+    @Bean
+    public RouteLocator BanklyCoreRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+
+        return routeLocatorBuilder.routes()
+                .route(p -> p.path("/banklycore/accounts/**")
+                        .filters(f -> f.rewritePath("/banklycore/accounts/(?<segment>.*)", "/${segment}")).uri("lb://ACCOUNTS"))
+
+                .route(p -> p.path("/banklycore/loans/**")
+                        .filters(f -> f.rewritePath("/banklycore/loans/(?<segment>.*)", "/${segment}")).uri("lb://LOANS"))
+
+                .route(p -> p.path("/banklycore/cards/**")
+                        .filters(f -> f.rewritePath("/banklycore/cards/(?<segment>.*)", "/${segment}")).uri("lb://CARDS")).build();
+    }
 
 }
