47f988caba206b1895d60d17f1bbbabc55708d5a
Kartikeya <kartikeyayash4@gmail.com>
2026-05-18 15:21:40 +0530

    Implemented Http timeout configurations

--- gatewayserver/src/main/java/com/example/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/example/gatewayserver/GatewayserverApplication.java
@@ -50,3 +50,20 @@ public class GatewayserverApplication {
 //http://localhost:8072/actuator/circuitbreakerevents?name=accountsCircuitBreaker ----> This to know about all the circuit Breaker Events
 
 // http://localhost:8072/actuator/circuitbreakers ---> this to get to know what state circuit Breaker State is in(Open, Close, Half Open )
+
+
+
+// ----------------- FOR PER-TIMEOUT configurations using Java DSL---------------------
+//
+//      @Bean
+//      public RouteLocator customRouteLocator(RouteLocatorBuilder routeBuilder){
+//         return routeBuilder.routes()
+//               .route("test1", r -> {
+//                  return r.host("*.somehost.org").and().path("/somepath")
+//                        .filters(f -> f.addRequestHeader("header1", "header-value-1"))
+//                        .uri("http://someuri")
+//                        .metadata(RESPONSE_TIMEOUT_ATTR, 200)
+//                        .metadata(CONNECT_TIMEOUT_ATTR, 200);
+//               })
+//               .build();
+//      }
