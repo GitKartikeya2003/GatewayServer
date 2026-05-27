31fba1511dee90536145df0b6bb5e35754afe63e
Kartikeya <kartikeyayash4@gmail.com>
2026-05-27 21:42:06 +0530

    How to change the default configuration of Circuit Breaker

--- gatewayserver/src/main/java/com/example/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/example/gatewayserver/GatewayserverApplication.java
@@ -1,73 +1,91 @@
-package com.example.gatewayserver;
+    package com.example.gatewayserver;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
+    import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+    import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+    import org.springframework.boot.SpringApplication;
+    import org.springframework.boot.autoconfigure.SpringBootApplication;
+    import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+    import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+    import org.springframework.cloud.client.circuitbreaker.Customizer;
+    import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+    import org.springframework.cloud.gateway.route.RouteLocator;
+    import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+    import org.springframework.context.annotation.Bean;
+    import org.springframework.http.HttpMethod;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
+    import java.time.Duration;
+    import java.time.LocalDateTime;
 
-@SpringBootApplication
-@EnableDiscoveryClient
-public class GatewayserverApplication {
+    @SpringBootApplication
+    @EnableDiscoveryClient
+    public class GatewayserverApplication {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        SpringApplication.run(GatewayserverApplication.class, args);
+            SpringApplication.run(GatewayserverApplication.class, args);
+
+        }
+
+
+        //Implementing Custom Routing in Spring Cloud gateway Server
+        @Bean
+        public RouteLocator BanklyCoreRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+
+            return routeLocatorBuilder.routes()
+                    .route(p -> p.path("/banklycore/accounts/**")
+                            .filters(f -> f.rewritePath("/banklycore/accounts/(?<segment>.*)", "/${segment}")
+                                    .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                    .circuitBreaker(config -> config.setName("accountsCircuitBreaker").setFallbackUri("forward:/contact-support")))
+                            .uri("lb://ACCOUNTS"))
+
+                    .route(p -> p.path("/banklycore/loans/**")
+                            .filters(f -> f.rewritePath("/banklycore/loans/(?<segment>.*)", "/${segment}")
+                                            .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                    .retry(retryConfig -> retryConfig.setRetries(3)
+                                            .setMethods(HttpMethod.GET)
+                                            .setBackoff(Duration.ofMillis(100),Duration.ofMillis(1000),2,true))
+                            )
+                            .uri("lb://LOANS"))
+
+                    .route(p -> p.path("/banklycore/cards/**")
+                            .filters(f -> f.rewritePath("/banklycore/cards/(?<segment>.*)", "/${segment}")
+                                            .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                    //   .circuitBreaker(config -> config.setName("cardsCircuitBreaker"))
+                            )
+                            .uri("lb://CARDS")).build();
+        }
+
+
+        @Bean
+        public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+            return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                    .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                    .timeLimiterConfig(
+                            TimeLimiterConfig.custom()
+                                    .timeoutDuration(Duration.ofSeconds(4))
+                                    .build()
+                    )
+                    .build());
+        }
 
     }
+    //http://localhost:8072/actuator/circuitbreakerevents?name=accountsCircuitBreaker ----> This to know about all the circuit Breaker Events
 
-
-    //Implementing Custom Routing in Spring Cloud gateway Server
-    @Bean
-    public RouteLocator BanklyCoreRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
-
-        return routeLocatorBuilder.routes()
-                .route(p -> p.path("/banklycore/accounts/**")
-                        .filters(f -> f.rewritePath("/banklycore/accounts/(?<segment>.*)", "/${segment}")
-                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
-                                .circuitBreaker(config -> config.setName("accountsCircuitBreaker").setFallbackUri("forward:/contact-support")))
-                        .uri("lb://ACCOUNTS"))
-
-                .route(p -> p.path("/banklycore/loans/**")
-                        .filters(f -> f.rewritePath("/banklycore/loans/(?<segment>.*)", "/${segment}")
-                                        .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
-                                .retry(retryConfig -> retryConfig.setRetries(3)
-                                        .setMethods(HttpMethod.GET)
-                                        .setBackoff(Duration.ofMillis(100),Duration.ofMillis(1000),2,true))
-                        )
-                        .uri("lb://LOANS"))
-
-                .route(p -> p.path("/banklycore/cards/**")
-                        .filters(f -> f.rewritePath("/banklycore/cards/(?<segment>.*)", "/${segment}")
-                                        .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
-                                //   .circuitBreaker(config -> config.setName("cardsCircuitBreaker"))
-                        )
-                        .uri("lb://CARDS")).build();
-    }
-
-}
-//http://localhost:8072/actuator/circuitbreakerevents?name=accountsCircuitBreaker ----> This to know about all the circuit Breaker Events
-
-// http://localhost:8072/actuator/circuitbreakers ---> this to get to know what state circuit Breaker State is in(Open, Close, Half Open )
+    // http://localhost:8072/actuator/circuitbreakers ---> this to get to know what state circuit Breaker State is in(Open, Close, Half Open )
 
 
 
-// ----------------- FOR PER-TIMEOUT configurations using Java DSL---------------------
-//
-//      @Bean
-//      public RouteLocator customRouteLocator(RouteLocatorBuilder routeBuilder){
-//         return routeBuilder.routes()
-//               .route("test1", r -> {
-//                  return r.host("*.somehost.org").and().path("/somepath")
-//                        .filters(f -> f.addRequestHeader("header1", "header-value-1"))
-//                        .uri("http://someuri")
-//                        .metadata(RESPONSE_TIMEOUT_ATTR, 200)
-//                        .metadata(CONNECT_TIMEOUT_ATTR, 200);
-//               })
-//               .build();
-//      }
+    // ----------------- FOR PER-TIMEOUT configurations using Java DSL---------------------
+    //
+    //      @Bean
+    //      public RouteLocator customRouteLocator(RouteLocatorBuilder routeBuilder){
+    //         return routeBuilder.routes()
+    //               .route("test1", r -> {
+    //                  return r.host("*.somehost.org").and().path("/somepath")
+    //                        .filters(f -> f.addRequestHeader("header1", "header-value-1"))
+    //                        .uri("http://someuri")
+    //                        .metadata(RESPONSE_TIMEOUT_ATTR, 200)
+    //                        .metadata(CONNECT_TIMEOUT_ATTR, 200);
+    //               })
+    //               .build();
+    //      }
